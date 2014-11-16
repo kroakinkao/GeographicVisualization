62c9a2db425df3dff7950764e90493bbd444a84c
Hong Chun <hongl@king.com>
2014-11-16 18:09:52 +0000

    added more code

--- server.js
+++ server.js
@@ -3,3 +3,8 @@ var express = require('express');
 var http = require('http');
 var fs = require('fs');
 var io = require('socket.io');
+
+//setup server
+var app = express();
+var server = http.Server(app);
+var socket = io(server);
